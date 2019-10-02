0c9abd45772a1beccebd50bb906f1b603c503b75
Pinaki <pinaki_ghosh1990@yahoo.com>
2019-10-02 23:33:25 +0530

    First commit with project structure

---
+++ src/main/groovy/com/example/inventory/InventoryApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.inventory
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class InventoryApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(InventoryApplication, args)
+	}
+
+}
